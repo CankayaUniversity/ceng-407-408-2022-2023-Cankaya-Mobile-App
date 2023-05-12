5b0606df978503c94c4ad7a800d70b913c59be62
Furkan <48026999+furkan-dogan@users.noreply.github.com>
2023-05-12 19:01:14 +0300

    Update Home.js

--- CankayaMobileApp/screens/Home.js
+++ CankayaMobileApp/screens/Home.js
@@ -1,25 +1,14 @@
 import {View, Text, SafeAreaView, Linking, TextInput, StyleSheet, TouchableOpacity} from "react-native";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import BackgroundAnimation from "../components/BackgroundAnimation";
 import Logo from "../components/Logo";
-import { useNavigation } from "@react-navigation/native";
-import { firebase } from "../config";
-import Registration from "./Registration";
-import {cankayaYellow} from "../src/constants";
+import {useNavigation} from "@react-navigation/native";
+import {firebase} from "../config";
 
 const Home = (props) => {
     const navigation = useNavigation();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-    let loginUser;
-    loginUser = async () => {
-        try {
-            await firebase.auth().signInWithEmailAndPassword(email, password);
-        } catch (error) {
-            alert(error.message);
-        }
-    };
 
     //forget password
     const forgetPassword = () => {
@@ -30,6 +19,38 @@ const Home = (props) => {
             alert(error)
         })
     }
+
+    const handleLogin = () => {
+        //debugger;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                userCredential.user.sendEmailVerification({
+                    handleCodeInApp: true,
+                    url: 'https://cankaya-mobile-app.firebaseapp.com',
+                })
+                    .then(() => {
+                        alert('Verification email sent')
+                    }).catch((error) => {
+                    alert(error.message)
+                })
+                    .then(() => {
+                        firebase.firestore().collection('users')
+                            .doc(firebase.auth().currentUser.uid)
+                            .set({
+                                email,
+                            })
+                    })
+                    .catch((error) => {
+                        alert(error.message)
+                    })
+
+            })
+            .catch((error) => {
+                        // If registration fails, handle the error appropriately
+                        alert(error.message);
+                    });
+
+    };
 
     return (
         <>
@@ -44,6 +65,7 @@ const Home = (props) => {
                                    onChangeText={(email) => setEmail(email)}
                                    autoCapitalize="none"
                                    autoCorrect={false}/>
+
                         <TextInput style={styles.input}
                                    placeholder="password"
                                    placeholderTextColor="black"
@@ -54,46 +76,44 @@ const Home = (props) => {
                     </View>
 
                     <View style={styles.buttonView}>
+
                         <TouchableOpacity style={styles.button}
-                                          onPress={() => loginUser(email, password)}>
+                                          onPress={() => {
+                                              handleLogin(email, password)
+                                              //debugger;
+                                              props.navigation.navigate("Login")
+                                          }}
+                        >
                             <Text style={styles.text1}>Login</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity style={[styles.button2]}
-                                          onPress={() => props.navigation.navigate("Registration")}>
-                            <Text style={styles.text2}>Registration</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.button}
-                                          onPress={() => props.navigation.navigate("Guest")}>
-                            <Text style={styles.text1}>Guest</Text>
-                        </TouchableOpacity>
                         <TouchableOpacity
-                        onPress={() => forgetPassword()}
-                        style={{
-                            backgroundColor: "rgba(255, 255, 255, 0.6)",
-                            borderRadius: 50,
-                            width: 150,
-                            height: 20,
-                            alignItems: "center",
-                            justifyContent: "center",
-                            marginVertical: 5,
-                        }}
+                            onPress={() => forgetPassword()}
+                            style={{
+                                backgroundColor: "rgba(255, 255, 255, 0.6)",
+                                borderRadius: 50,
+                                width: 150,
+                                height: 20,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                marginVertical: 5,
+                            }}
                         >
-                        <Text style={{ fontWeight: "bold", fontSize: 12 }}>Forget Password?</Text>
-                    </TouchableOpacity>
+                            <Text style={{fontWeight: "bold", fontSize: 12}}>Forget Password?</Text>
+                        </TouchableOpacity>
 
                     </View>
                 </View>
                 <View>
                     <View style={styles.backgroundFrame}>
                         <Text style={{color: "black", fontWeight: "bold", fontSize: 13}}>
-                            Çankaya Üniversitesi Öğrenci Webmail Servisi
+                            Cankaya University Student Webmail Service
                         </Text>
                         <Text
                             style={{color: "blue"}}
                             onPress={() => Linking.openURL("https://bim.cankaya.edu.tr/")}
                         >
-                            Destek İsteyin !
+                            Request Support!
                         </Text>
                     </View>
                 </View>
@@ -106,22 +126,22 @@ export default Home;
 
 const styles = StyleSheet.create({
     container: {
-      alignItems: "center",
+        alignItems: "center",
     },
     input: {
         borderRadius: 100,
         color: "black",
-        paddingHorizontal: 10,
+        paddingHorizontal: 20,
         width: 350,
         height: "19%",
         backgroundColor: "rgb(220,220,220)",
-        marginVertical: 5,
+        marginVertical: 10,
     },
     inputView: {
-      marginTop: 40,
+        marginTop: 40,
     },
     button: {
-        backgroundColor: cankayaYellow,
+        backgroundColor: '#E2D102',
         borderRadius: 100,
         alignItems: "center",
         width: 150,
@@ -137,7 +157,7 @@ const styles = StyleSheet.create({
         marginVertical: 5,
     },
     buttonView: {
-      alignItems: "center",
+        alignItems: "center",
     },
     backgroundFrame: {
         backgroundColor: "rgba(255, 255, 255, 0.6)",
