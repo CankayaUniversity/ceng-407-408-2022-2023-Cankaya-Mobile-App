2df74ea9952dcae17a79922910b92eb2834986e1
İrem Bakar <116024439+irembakar@users.noreply.github.com>
2023-05-13 15:06:04 +0300

    Updated Survey.js

--- CankayaMobileApp/navigation/screens/Survey.js
+++ CankayaMobileApp/navigation/screens/Survey.js
@@ -1,10 +1,52 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Survey(navigation) {
+const Survey = () => {
+  const navigation = useNavigation();
+  const [selectedSurvey, setSelectedSurvey] = useState('survey1');
+
+  const handleSurveySelection = (value) => {
+    setSelectedSurvey(value);
+    if (value === 'survey1') {
+      navigation.navigate('Survey1');
+    } else if (value === 'survey2') {
+      navigation.navigate('Survey2');
+    } else if (value === 'survey3') {
+      navigation.navigate('Survey3');
+    }
+};
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text onPress={() => navigation.navigate("Home")}>Survey</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Survey</Text>
+      <Picker
+        selectedValue={selectedSurvey}
+        onValueChange={handleSurveySelection}
+        style={styles.picker}
+      >
+        <Picker.Item label="Survey 1" value="Survey1" />
+        <Picker.Item label="Survey 2" value="Survey2" />
+        <Picker.Item label="Survey 3" value="Survey3" />
+      </Picker>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  picker: {
+    width: '80%',
+  },
+});
+export default Survey;
