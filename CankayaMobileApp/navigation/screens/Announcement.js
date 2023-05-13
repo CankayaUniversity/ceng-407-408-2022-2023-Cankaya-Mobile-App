5a010eb985bcbf7ce749f6b5b0b27f637ee8f498
Beste Alptekin <99659331+bestealptekin@users.noreply.github.com>
2023-05-13 19:05:41 +0300

    Update about issue in Announcement.js

--- CankayaMobileApp/navigation/screens/Announcement.js
+++ CankayaMobileApp/navigation/screens/Announcement.js
@@ -1,12 +1,10 @@
 import React from "react";
 import {WebView} from "react-native-webview";
 import {SafeAreaView} from "react-native-safe-area-context";
-import Back from "../../components/Back";
 
 export default function Announcement(navigation) {
     return (
         <SafeAreaView style={{flex: 1}}>
-            <Back/>
             <WebView
                 source={{
                     uri: "https://www.cankaya.edu.tr/duyuru/",
