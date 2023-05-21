c647906ec9726e445b1706122834b71203a8ca2d
Furkan <48026999+furkan-dogan@users.noreply.github.com>
2023-05-21 17:14:04 +0300

    Add files via upload

--- CankayaMobileApp/App.js
+++ CankayaMobileApp/App.js
@@ -1,46 +1,47 @@
 import React, {useEffect, useState} from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {firebase} from "./config";
+import { signOut } from "firebase/auth";
+
+import {firebaseAuth} from "./src/utils/firebaseHelper";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Guest from "./screens/Guest";
-import {View} from "react-native";
-import TakeA from "./screens/TakeA";
-import ViewA from "./screens/ViewA";
+import {findUserByEmail} from "./src/firestoreQueries";
+import AppContext, {useUser} from "./src/context";
 
 const Stack = createNativeStackNavigator();
 
-function App() {
-    const [initializing, setInitializing] = useState(true);
-    const [user, setUser] = useState();
+const Main = () => {
+    const {user, setUser} = useUser();
 
-    //Handle user state changes
-    function onAuthStateChanged(user) {
-        setUser(user);
-        if (initializing) setInitializing(false);
+    const onAuthStateChanged = async (user) => {
+        if (user?.emailVerified) {
+            const user_ = await findUserByEmail({email: user.email});
+            setUser(user_);
+        }
     }
 
     useEffect(() => {
-        return firebase.auth().onAuthStateChanged(onAuthStateChanged);
+        // signOut(firebaseAuth);
+        return firebaseAuth.onAuthStateChanged(onAuthStateChanged);
     }, []);
 
-    if (initializing) return <View><NavigationContainer/></View>
-        ;
-
-    if (!user) {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Guest" component={Guest} />
-        <Stack.Screen name="TakeA" component={TakeA} />
-        <Stack.Screen name="ViewA" component={ViewA} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+    if (!user || !user.emailVerified) {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
+                    <Stack.Screen name="Home" component={Home}/>
+                    <Stack.Screen name="Login" component={Login}/>
+                    <Stack.Screen
+                        options={{headerShown: false}}
+                        name="Guest"
+                        component={Guest}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 
     return (
         <NavigationContainer>
@@ -54,5 +55,14 @@ function App() {
         </NavigationContainer>
 
     );
+};
+
+function App() {
+    return (
+        <AppContext>
+            <Main />
+        </AppContext>
+    )
 }
+
 export default App;
