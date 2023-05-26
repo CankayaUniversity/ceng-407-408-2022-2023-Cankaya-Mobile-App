16447d203f9ce9400f43fdf62c1fc5a99f67b194
İrem Bakar <116024439+irembakar@users.noreply.github.com>
2023-05-26 19:47:50 +0300

    Updated Survey3.js

--- CankayaMobileApp/screens/Survey3.js
+++ CankayaMobileApp/screens/Survey3.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
-import { ScrollView, Text, TouchableOpacity, ImageBackground, TextInput, StyleSheet } from 'react-native';
-import { RadioButton } from 'react-native-paper';
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+  View,
+} from 'react-native';
 
 const Survey3 = () => {
   const [question1, setQuestion1] = useState('');
@@ -20,60 +27,38 @@ const Survey3 = () => {
   };
 
   const handleSubmit = () => {
-    // Handle submit logic here
-  }
-
-  // const handleSubmit = () => {
-  //   console.log('Attendance:', question1);
-  //   console.log('CPGA:', question2);
-  //   console.log('Question 3:', question3);
-  //   console.log('Question 4:', question4);
-  // };
+    // Handle submit logic
+  };
 
   return (
-    <ScrollView>
-        <Text style={{backgroundColor: '#ADD8E6', fontSize: 18, fontWeight: 'bold'}}>Question 1:</Text>
-      <TextInput
-        value={question4}
-        onChangeText={handleQuestion4Change}
-        style={{
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        padding: 10,
-        marginVertical: 10,
-        fontSize: 14,
-        }}
-    />
-        <Text style={{backgroundColor: '#ADD8E6', fontSize: 18, fontWeight: 'bold'}}>Question 2:</Text>
-      <TextInput
-        value={question4}
-        onChangeText={handleQuestion4Change}
-        style={{
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        padding: 10,
-        marginVertical: 10,
-        fontSize: 14,
-        }}
-    />
-        <Text style={{backgroundColor: '#ADD8E6', fontSize: 18, fontWeight: 'bold'}}>Question 3:</Text>
-      <TextInput
-        value={question4}
-        onChangeText={handleQuestion4Change}
-        style={{
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 5,
-        padding: 10,
-        marginVertical: 10,
-        fontSize: 14,
-        }}
-    />
-      {/* <TouchableOpacity onPress={handleSubmit}>
-        <Text>Submit</Text>
-      </TouchableOpacity> */}
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.questionContainer}>
+        <Text style={styles.questionTitle}>1. Name 3 most favourite things about Çankaya University:</Text>
+        <TextInput
+          value={question1}
+          onChangeText={handleQuestion1Change}
+          style={styles.inputLarge}
+        />
+      </View>
+
+      <View style={styles.questionContainer}>
+        <Text style={styles.questionTitle}>2. Name 3 least favourite things about Çankaya University:</Text>
+        <TextInput
+          value={question2}
+          onChangeText={handleQuestion2Change}
+          style={styles.inputLarge}
+        />
+      </View>
+
+      <View style={styles.questionContainer}>
+        <Text style={styles.questionTitle}>3. How would you evaluate your experience at Çankaya University?</Text>
+        <TextInput
+          value={question3}
+          onChangeText={handleQuestion3Change}
+          style={styles.inputLarge}
+        />
+      </View>
+
       <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
         <Text style={styles.submitButtonText}>Submit</Text>
       </TouchableOpacity>
@@ -81,19 +66,47 @@ const Survey3 = () => {
   );
 };
 
-// const styles = StyleSheet.create({
-//   submitButton: {
-//     backgroundColor: '#ADD8E6',
-//     borderRadius: 5,
-//     padding: 10,
-//     alignItems: 'center',
-//     marginVertical: 20,
-//   },
-//   submitButtonText: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     color: 'black',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+  },
+  questionContainer: {
+    marginBottom: 20,
+  },
+  questionTitle: {
+    backgroundColor: '#ADD8E6',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 14,
+  },
+  inputLarge: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 14,
+    height: 150, // Set the height for the enlarged text
+    textAlignVertical: 'top', // Place the text at the top of the input
+  },
+  submitButton: {
+    backgroundColor: '#ADD8E6',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  submitButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
 
 export default Survey3;
