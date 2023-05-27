2253c6e479e3552d85e645202df92e85ea8175d6
MertcanSeyda <40696700+MertcanSeyda@users.noreply.github.com>
2023-05-27 14:15:59 +0300

    Update TakeA.js
    The drop-down list has been changed and the qr code generation button has been created.

--- CankayaMobileApp/screens/TakeA.js
+++ CankayaMobileApp/screens/TakeA.js
@@ -1,49 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
-import DropDownPicker from 'react-native-dropdown-picker';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-
-const SelectionScreen = () => {
-  const [selectedValue, setSelectedValue] = useState(null);
-  const [qrValue, setQrValue] = useState(null);
-
-  const items = [
-    { label: 'Seçenek 1', value: 'option1' },
-    { label: 'Seçenek 2', value: 'option2' },
-    { label: 'Seçenek 3', value: 'option3' },
-    { label: 'Seçenek 4', value: 'option4' },
-    { label: 'Seçenek 5', value: 'option5' }
+const DropdownList = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [
+    { label: 'Option 1', value: 'option1' },
+    { label: 'Option 2', value: 'option2' },
+    { label: 'Option 3', value: 'option3' },
+    { label: 'Option 4', value: 'option4' },
   ];
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  useEffect(() => {
-    if (selectedValue) {
-      // Seçilen değer değiştiğinde yeni bir QR kodu oluştur
-      const randomQR = Math.floor(Math.random() * 1000000).toString();
-      setQrValue(randomQR);
-    }
-  }, [selectedValue]);
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option.value);
+    setIsDropdownOpen(false);
+  };
+
+  const handleCreateQR = () => {
+    
+    console.log('QR kod oluşturuldu');
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Please choose a course:</Text>
-      <DropDownPicker
-        items={items}
-        defaultValue={selectedValue}
-        containerStyle={styles.dropdownContainer}
-        style={styles.dropdown}
-        itemStyle={styles.dropdownItem}
-        labelStyle={styles.dropdownLabel}
-        onChangeItem={item => setSelectedValue(item.value)}
-        placeholder="Select a course"
-      />
-      {qrValue && (
-        <View style={styles.qrContainer}>
-          <QRCode value={qrValue} size={200} />
-          <Text style={styles.qrText}>{qrValue}</Text>
+      <TouchableOpacity
+        style={styles.dropdownButton}
+        onPress={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        <Text style={styles.dropdownButtonText}>
+          {selectedOption ? selectedOption : 'Seçenekler'}
+        </Text>
+        <Icon
+          name={isDropdownOpen ? 'chevron-up' : 'chevron-down'}
+          size={20}
+          color="#000"
+        />
+      </TouchableOpacity>
+      {isDropdownOpen && (
+        <View style={styles.dropdownList}>
+          {options.map((option) => (
+            <TouchableOpacity
+              key={option.value}
+              style={styles.dropdownListItem}
+              onPress={() => handleOptionSelect(option)}
+            >
+              <Text style={styles.dropdownListItemText}>{option.label}</Text>
+            </TouchableOpacity>
+          ))}
         </View>
       )}
+      <TouchableOpacity
+        style={styles.createQRButton}
+        onPress={handleCreateQR}
+      >
+        <Text style={styles.createQRButtonText}>Create QR</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -53,36 +65,50 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff'
   },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  dropdownContainer: {
-    height: 40,
-    width: 200,
-    marginTop: 20
-  },
-  dropdown: {
-    backgroundColor: '#fafafa'
-  },
-  dropdownItem: {
-    justifyContent: 'flex-start'
-  },
-  dropdownLabel: {
-    fontSize: 16,
-    textAlign: 'left',
-    color: '#000'
-  },
-  qrContainer: {
+  dropdownButton: {
+    flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 20
+    justifyContent: 'space-between',
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    marginBottom: 20,
   },
-  qrText: {
+  dropdownButtonText: {
     fontSize: 16,
-    marginTop: 10
-  }
+  },
+  dropdownList: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    marginBottom: 20,
+  },
+  dropdownListItem: {
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  dropdownListItemText: {
+    fontSize: 16,
+  },
+  createQRButton: {
+    width: 200,
+    height: 40,
+    backgroundColor: '#ccc',
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  createQRButtonText: {
+    fontSize: 16,
+    color: '#fff',
+  },
 });
 
-export default SelectionScreen;
+export default DropdownList;
