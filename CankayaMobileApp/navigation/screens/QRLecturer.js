89ca624e7163c67ae818324d2a661f7de51bcdd2
Furkan <48026999+furkan-dogan@users.noreply.github.com>
2023-05-27 11:05:07 +0300

    Update QRLecturer.js

--- CankayaMobileApp/navigation/screens/QRLecturer.js
+++ CankayaMobileApp/navigation/screens/QRLecturer.js
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 12,
     },
     button: {
         flex: 1,
